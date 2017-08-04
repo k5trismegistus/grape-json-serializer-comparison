fdb464f585dea8232d5dbb3da12b77a1b8ae79b7
keigo_yamamoto <keigo_yamamoto@dwango.co.jp>
2017-08-04 12:20:14 +0900

    chore: created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do |i|
+  user = User.create!(name: "user#{i}")
+
+  10.times do |j|
+    Article.create!(
+      title: "article#{j}",
+      content: "this article is written by #{user.name}. Enjoy!",
+      user_id: user.id
+    )
+  end
+end
+
