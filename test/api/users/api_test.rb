bd4e6afa1937a9f743553db26cdae94d915f377f
keigo_yamamoto <keigo_yamamoto@dwango.co.jp>
2017-08-09 14:38:54 +0900

    add test script

---
+++ test/api/users/api_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+require 'benchmark'
+
+class UsersAPITest < ActionDispatch::IntegrationTest
+  REQUEST_TRIAL = 10000
+
+  REQUEST_IDS = (1..REQUEST_TRIAL).map do
+    rand(1..100)
+  end
+
+  test 'jbuilder' do
+    get "/api/users/1/jbuilder"
+
+    result = Benchmark.realtime do
+      REQUEST_IDS.each do |id|
+        get "/api/users/#{id}/jbuilder"
+      end
+    end
+
+    puts "jbuilder: #{result}"
+  end
+
+  test 'rabl' do
+    get "/api/users/1/jbuilder"
+
+    result = Benchmark.realtime do
+      REQUEST_IDS.each do |id|
+        get "/api/users/#{id}/jbuilder"
+      end
+    end
+
+    puts "rabl: #{result}"
+  end
+
+  test 'ams' do
+    get "/api/users/1/jbuilder"
+
+    result = Benchmark.realtime do
+      REQUEST_IDS.each do |id|
+        get "/api/users/#{id}/ams"
+      end
+    end
+
+    puts "ams: #{result}"
+  end
+
+  test 'ge' do
+    get "/api/users/1/jbuilder"
+
+    result = Benchmark.realtime do
+      REQUEST_IDS.each do |id|
+        get "/api/users/#{id}/ge"
+      end
+    end
+
+    puts "ge: #{result}"
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,25 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    100.times do |i|
+    user = User.create!(name: "user#{i}")
 
-  # Add more helper methods to be used by all tests here...
+    10.times do |j|
+      Article.create!(
+        title: "article#{j}",
+        content: "this article is written by #{user.name}. Enjoy!",
+        user_id: user.id
+      )
+    end
+  end
+
+  User.all.each do |user|
+    article_ids = (1..1000).to_a.sample(300)
+    article_ids.each do |article_id|
+      Bookmark.create!(
+        user_id: user.id,
+        article_id: article_id
+      )
+    end
+  end
 end
